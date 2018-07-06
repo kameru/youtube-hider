5457dd3341cd6f4b28d52693e7e176891dbe7244
kameru <kameru1208@gmail.com>
2018-07-06 17:47:12 +0900

    declare link variable only popup.js excuted

--- popup.js
+++ popup.js
@@ -2,17 +2,17 @@ let thumbCheckbox = document.getElementById("hide-thumb");
 let videoCheckbox = document.getElementById("hide-video");
 
 thumbCheckbox.addEventListener('change', () => {
-    chrome.storage.sync.set({thumb: thumbCheckbox.value});
+    chrome.storage.sync.set({thumb: thumbCheckbox.checked});
     onCheckChange('thumb');
 });
 videoCheckbox.addEventListener('change', () => {
-    chrome.storage.sync.set({video: videoCheckbox.value});
+    chrome.storage.sync.set({video: videoCheckbox.checked});
     onCheckChange('video');
 });
 
 function onCheckChange(type) {
     chrome.storage.sync.get(type, function (value) {
-        if (value) {
+        if (value[type]) {
             addStylesheet(type);
         } else {
             removeStylesheet(type)
@@ -23,11 +23,11 @@ function onCheckChange(type) {
 function addStylesheet(type) {
     chrome.tabs.executeScript({
         code: `    
-            let link = document.getElementById("${type}-hider");
+            link = document.getElementById("${type}-hider");
             if (!link) {
                 link = document.createElement("link");
                 link.id = "${type}-hider";
-                link.href = "hide_${type}.css";
+                link.href = "${chrome.extension.getURL('hide_' + type + '.css')}";
                 link.type = "text/css";
                 link.rel = "stylesheet";
                 document.getElementsByTagName("head")[0].appendChild(link);
@@ -39,7 +39,7 @@ function addStylesheet(type) {
 function removeStylesheet(type) {
     chrome.tabs.executeScript({
         code: `
-        let link = document.getElementById("${type}-hider");
+        link = document.getElementById("${type}-hider");
         if (link) {
             link.remove();
         }
@@ -50,4 +50,8 @@ function removeStylesheet(type) {
 onCheckChange('thumb');
 onCheckChange('video');
 
-
+chrome.tabs.executeScript({
+    code: `if (window.link === undefined) {
+    let link = ''
+    };`
+});

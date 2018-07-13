d6aafaf3877bbc6a0b54e5f2a908ca3644d4fdc4
kameru <kameru1208@gmail.com>
2018-07-13 18:31:35 +0900

    set youtube style without open popup

---
+++ hidingController.js
@@ -0,0 +1,41 @@
+function onCheckChange(type, value) {
+    if (value) {
+        addStylesheet(type);
+    } else {
+        removeStylesheet(type)
+    }
+}
+
+let link = '';
+
+function addStylesheet(type) {
+    link = document.getElementById(type + '-hider');
+    if (!link) {
+        link = document.createElement("link");
+        link.id = `${type}-hider`;
+        link.href = chrome.extension.getURL(`hide_${type}.css`);
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        document.getElementsByTagName("head")[0].appendChild(link);
+    }
+}
+
+function removeStylesheet(type) {
+    link = document.getElementById(`${type}-hider`);
+    if (link) {
+        link.remove();
+    }
+}
+
+chrome.storage.sync.get(['thumb', 'video'], function(result) {
+    onCheckChange('thumb', result.thumb);
+    onCheckChange('video', result.video);
+});
+
+
+chrome.storage.onChanged.addListener(function (changes) {
+   for (let type in changes) {
+       let value = changes[type].newValue;
+       onCheckChange(type, value);
+   }
+});
--- popup.js
+++ popup.js
@@ -3,55 +3,7 @@ let videoCheckbox = document.getElementById("hide-video");
 
 thumbCheckbox.addEventListener('change', () => {
     chrome.storage.sync.set({thumb: thumbCheckbox.checked});
-    onCheckChange('thumb');
 });
 videoCheckbox.addEventListener('change', () => {
     chrome.storage.sync.set({video: videoCheckbox.checked});
-    onCheckChange('video');
-});
-
-function onCheckChange(type) {
-    chrome.storage.sync.get(type, function (value) {
-        if (value[type]) {
-            addStylesheet(type);
-        } else {
-            removeStylesheet(type)
-        }
-    });
-}
-
-function addStylesheet(type) {
-    chrome.tabs.executeScript({
-        code: `    
-            link = document.getElementById("${type}-hider");
-            if (!link) {
-                link = document.createElement("link");
-                link.id = "${type}-hider";
-                link.href = "${chrome.extension.getURL('hide_' + type + '.css')}";
-                link.type = "text/css";
-                link.rel = "stylesheet";
-                document.getElementsByTagName("head")[0].appendChild(link);
-            }
-            `
-        })
-}
-
-function removeStylesheet(type) {
-    chrome.tabs.executeScript({
-        code: `
-        link = document.getElementById("${type}-hider");
-        if (link) {
-            link.remove();
-        }
-        `
-    })
-}
-
-onCheckChange('thumb');
-onCheckChange('video');
-
-chrome.tabs.executeScript({
-    code: `if (window.link === undefined) {
-    let link = ''
-    };`
 });

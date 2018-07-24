eb6cc735cfbdb2d3d20ddc0999590af758ae93c9
kameru <kameru1208@gmail.com>
2018-07-24 18:29:51 +0900

    prevent initializing checkbox

--- popup.js
+++ popup.js
@@ -1,6 +1,11 @@
 let thumbCheckbox = document.getElementById("hide-thumb");
 let videoCheckbox = document.getElementById("hide-video");
 
+chrome.storage.sync.get(['thumb', 'video'], function(result) {
+    thumbCheckbox.checked = result.thumb;
+    videoCheckbox.checked = result.video;
+});
+
 thumbCheckbox.addEventListener('change', () => {
     chrome.storage.sync.set({thumb: thumbCheckbox.checked});
 });
